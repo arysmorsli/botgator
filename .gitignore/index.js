cd8900eba25927f9a3f1bd575f7561083cb9e217
arysmorsli <33126723+arysmorsli@users.noreply.github.com>
2018-08-20 01:50:47 -0400

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,7 @@
+{
+  "name": "discord.js",
+  "version": "12.0.0-dev",
+  "description": "A powerful library for interacting with the Discord API",
+    "docs:test": "docgen --source src --custom docs/index.yml",
+    "lint": "eslint src *.js",
+    "lint:fix": "eslint --fix src",
